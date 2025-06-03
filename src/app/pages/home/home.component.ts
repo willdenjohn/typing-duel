2354a00c7bb0d6435f0c660485edaa480a2d0c73
willdenjohn <willdenaguiar@gmail.com>
2025-06-03 14:13:06 -0300

    feat(home): implement responsive home screen with Tailwind layout and navigation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  startGame() {
+    this.router.navigate(['/game']);
+  }
 }
