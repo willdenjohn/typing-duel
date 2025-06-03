00bd62bb6da7561d42369b1047f7017352c9e7f9
willdenjohn <willdenaguiar@gmail.com>
2025-06-03 14:30:11 -0300

    feat(game-over): create final score screen with dynamic message and restart option

--- src/app/pages/game-over/game-over.component.ts
+++ src/app/pages/game-over/game-over.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game-over',
-  imports: [],
+  standalone: true,
   templateUrl: './game-over.component.html',
-  styleUrl: './game-over.component.scss'
 })
 export class GameOverComponent {
+  private router = inject(Router);
 
+  score = 0;
+  message = '';
+
+  constructor() {
+    const nav = this.router.getCurrentNavigation();
+    this.score = nav?.extras.state?.['score'] ?? 0;
+
+    this.message = this.getMessage(this.score);
+  }
+
+  getMessage(score: number): string {
+    if (score >= 10) return 'Você é um ninja da digitação! 🚀';
+    if (score >= 5) return 'Mandou bem! Mas dá pra melhorar 😎';
+    return 'Foi só aquecimento, tenta de novo! 🔁';
+  }
+
+  restartGame() {
+    this.router.navigate(['/']);
+  }
 }
--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -29,7 +29,7 @@ export class GameComponent implements OnInit {
   }
 
   startTimer() {
-    interval(1000)
+    interval(100)
       .pipe(takeWhile(() => this.timeLeft > 0))
       .subscribe(() => {
         this.timeLeft--;
