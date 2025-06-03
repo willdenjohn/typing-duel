1301f71616169141a86bdfaa0b0e7f46df66e4c5
willdenjohn <willdenaguiar@gmail.com>
2025-06-03 14:25:05 -0300

    refactor(game): apply inject() API and clean component structure

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { interval, takeWhile } from 'rxjs';
 
 @Component({
   selector: 'app-game',
-  imports: [],
+  standalone: true,
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  imports: [FormsModule]
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  private router = inject(Router);
 
+  currentWord = '';
+  userInput = '';
+  score = 0;
+  timeLeft = 60;
+
+  words = [
+    'angular', 'component', 'typescript', 'rxjs', 'template',
+    'service', 'module', 'directive', 'router', 'state',
+    'binding', 'observable', 'input', 'output', 'form'
+  ];
+
+  ngOnInit(): void {
+    this.nextWord();
+    this.startTimer();
+  }
+
+  startTimer() {
+    interval(1000)
+      .pipe(takeWhile(() => this.timeLeft > 0))
+      .subscribe(() => {
+        this.timeLeft--;
+        if (this.timeLeft === 0) {
+          this.router.navigate(['/game-over'], {
+            state: { score: this.score }
+          });
+        }
+      });
+  }
+
+  nextWord() {
+    const randomIndex = Math.floor(Math.random() * this.words.length);
+    this.currentWord = this.words[randomIndex];
+  }
+
+  checkWord() {
+    if (this.userInput.trim().toLowerCase() === this.currentWord.toLowerCase()) {
+      this.score++;
+      this.nextWord();
+    } else {
+      this.score--;
+    }
+    this.userInput = '';
+  }
 }
